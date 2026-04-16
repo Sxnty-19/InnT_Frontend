bf9968ab771fa103f83b2576e53eb2d1b4a23d18
Sxnty-19 <santymaciasmarquez21@gmail.com>
2026-04-15 21:02:55 -0500

    Add pages/c-reservas-h

--- src/app/pages/c-reservas-h/c-reservas-h.ts
+++ src/app/pages/c-reservas-h/c-reservas-h.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar';
+import { NavbarA } from '../../components/navbar-a/navbar-a';
+import { Footer } from '../../components/footer/footer';
+import { Reserva as ReservaService } from '../../services/reserva';
+import { Reserva } from '../../interfaces/reserva';
 
 @Component({
   selector: 'app-c-reservas-h',
-  imports: [],
+  imports: [CommonModule, Navbar, NavbarA, Footer],
   templateUrl: './c-reservas-h.html',
   styleUrl: './c-reservas-h.css',
 })
-export class CReservasH {
+export class CReservasH implements OnInit {
+  historial: Reserva[] = [];
+  error = '';
+  isLoading = true;
+  isLoaded = false;
 
+  constructor(private cd: ChangeDetectorRef, private reservaservice: ReservaService) { }
+
+  ngOnInit(): void {
+    this.cargarHistorial();
+    this.cd.detectChanges();
+  }
+
+  cargarHistorial(): void {
+    this.isLoading = true;
+    this.error = '';
+
+    this.reservaservice.getReservasTerminadas().subscribe({
+      next: (data: any) => {
+        this.historial = data.data || [];
+        if (this.historial.length === 0) {
+          this.error = 'No tienes reservas finalizadas.';
+        }
+        this.isLoaded = true;
+        this.isLoading = false;
+        this.cd.detectChanges();
+      },
+      error: (err) => {
+        console.error(err);
+        this.error = 'Error al cargar historial.';
+        this.isLoading = false;
+        this.cd.detectChanges();
+      }
+    });
+  }
 }
